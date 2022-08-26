dbdabdf915cd69398e6332dba930427f79d26143
César Calafrioli <46635135+cesarcalafrioli@users.noreply.github.com>
2022-08-26 14:39:43 -0300

    app.py creation
    Creating the application file.

---
+++ app.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+def main():
+    st.title('PDF CONVERTER')
+    st.subheader('')
+    st.subheader('Conversor pdf para o formato txt')
+
+if __name__ == "__main__":
+    main()
