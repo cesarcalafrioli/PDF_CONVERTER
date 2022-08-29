f29fc5d617289238a312423858ddf65fbcc88b9b
César Calafrioli <46635135+cesarcalafrioli@users.noreply.github.com>
2022-08-28 22:49:20 -0300

    close #1, fixe #2, funcionalidade ler arquivo
    Adicionando a funcionalidade de ler o arquivo e o recurso drag and drop do streamlit.

--- app.py
+++ app.py
@@ -1,9 +1,20 @@
 import streamlit as st
 
+def load_doc():
+    document = st.file_uploader("", type=["pdf"])
+    if document is not None:
+
+        # Exibindo os detalhes
+        file_details = {"filename":document.name}
+        st.write(file_details)
+
 def main():
     st.title('PDF CONVERTER')
     st.subheader('')
     st.subheader('Conversor pdf para o formato txt')
+    st.subheader('')
+    st.text('Arraste um arquivo pdf para realizar a conversão')
+    load_doc()
 
 if __name__ == "__main__":
     main()
