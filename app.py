29b82d2a1820a2109cbb4f9194c22c9c857ee51c
César Calafrioli <46635135+cesarcalafrioli@users.noreply.github.com>
2022-09-03 23:23:39 -0300

    fixes #3 - Converter pdf para txt
    Adicionando a funcionalidade de converter arquivos PDF para o formato TXT.

--- app.py
+++ app.py
@@ -3,6 +3,33 @@
 # Desenvolvido por César Calafrioli
 #
 import streamlit as st
+from PyPDF2 import PdfFileReader
+
+# Converter o arquivo pdf em formato txt
+def conv_doc(file):
+    
+    pdfFile = open(file, "rb")
+
+    # Criando o objeto do arquivo pdf
+    pdfReader = PdfFileReader(pdfFile)
+
+    # Recebendo o número de páginas
+    count = pdfReader.numPages
+
+    # Convertendo o arquivo PDF para o formato TXT
+    for i in range(0+1, count):
+        pageObj = pdfReader.getPage(i)
+        text = pageObj.extractText()
+        textFile = open(r"./teste.txt","a",encoding="utf-16")
+        textFile.writelines(text)
+
+        # Exibindo o progresso da conversão
+        #latest_iteration.text(f'Convertendo {math.ceil((float(float(i)+1)/floaat(numPages))*100)} %')
+        
+    pdfFile.close()
+
+    st.write(count)
+
 
 # Ler o arquivo pdf
 def load_doc():
@@ -12,6 +39,15 @@ def load_doc():
         # Exibindo os detalhes
         file_details = {"filename":document.name}
         st.write(file_details)
+
+        # Botão para converter o arquivo PDF
+        btnConv = st.button('Converter para TXT')
+        
+        # Converte o arquivo para TXT após o usuário clicar no botão
+        if btnConv:
+            with open(document.name,"wb") as f:
+                f.write(document.getbuffer())
+            conv_doc(document.name)
 
 def main():
     st.title('PDF CONVERTER')
